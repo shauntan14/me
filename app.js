cb9c88a34f7a2485247924d5110ea55961620ebf
shaun <shauntan14@gmail.com>
2016-10-16 19:01:40 -0700

    js

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 $(document).ready(function() {
-   $('school').click(function() {
-        $(this).fadeOut('fast');
-   });
-});
-
-
-
-
-
-
-
-
-/*$(document).ready(function() {
-  $('school').hover(function() {
+  $('#schoolimg').hide();
+  $('#breakfastvideo').hide();
+  $('#slider').hide();
+  $('#work-desc').hide();
+  $('#school').hover(function() {
     $(this).addClass('damp');
   },
     function() {
       $(this).removeClass('damp');
     }
   );
+
+   $("#school").click(function() {
+        $('#schoolimg').fadeIn('slow');
+   });
+   $("#videos").click(function() {
+        $('#breakfastvideo').fadeIn('slow');
+   });
+   $("#photography").click(function() {
+        $('#slider').fadeIn('slow');
+   });
+   $("#work").click(function() {
+        $('#work-desc').fadeIn('slow');
+   });
+
+
 });
-*/
